d45bf77af14b68f656de8470bc3d1440bbb35249
Yusuf Kaan Çelik <84715164+yusufkaancelik@users.noreply.github.com>
2023-12-20 00:13:16 +0300

    Update Sports-Branch-Selector.py

--- Sports-Branch-Selector.py
+++ Sports-Branch-Selector.py
@@ -6,13 +6,13 @@ brnslr=["Futbol","Yüzme","Handball","Basketbol","Jimnastik"]
 k={"nihal","ipek","özden"}
 e={"enes","ufuk","ulaş","burak","caner"}
 isim_gir= input("adınızı girin:").lower()
+if isim_gir in k and e:  
 yas_gir= int(input("yaşınızı girin:"))
 ss=0
 for branslar in brnslr:
   ss=ss+1
   print(ss,branslar)
 brans_gir= input("seçmek isteiğiniz branşı yazınız:").lower()
-if isim_gir in k:
   if brans_gir == "futbol" or brans_gir== "1":
     kontrol= input("futbol sertifikanız var mı [e/h] : ").lower()
     if kontrol == "e":
@@ -21,22 +21,6 @@ if isim_gir in k:
       print("bu branşa uygun değilsiniz")
 
   elif brans_gir == "yüzme" or brans_gir == "1" and yas_gir <=6:uygun()
-
-  elif brans_gir == "handball" or brans_gir == "3" and yas_gir <=8:uygun()
-
-  elif brans_gir == "basketbol" or brans_gir == "4" and 20 <= yas_gir <= 30 :uygun()
-
-  elif brans_gir == "jimnastik" or brans_gir == "5":degil()
-
-  else:degil()
-
-elif isim_gir in e:
-  if brans_gir == "futbol" or brans_gir== "1":
-    kontrol= input("futbol sertifikanız var mı [e/h] :").lower()
-    if kontrol == "e":uygun()
-    else:degil()
-
-  elif brans_gir == "yüzme" or brans_gir == "2" and yas_gir <=6:uygun()
 
   elif brans_gir == "handball" or brans_gir == "3" and yas_gir <=8:uygun()
 
